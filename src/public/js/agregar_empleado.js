9abd2414b60f6d696226d48c48aadae6fca64af6
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-09-24 16:42:24 -0500

    Pequeños cambios de estilo y comienzo de metodo para crear empleado

---
+++ src/public/js/agregar_empleado.js
@@ -0,0 +1,80 @@
+document.addEventListener('DOMContentLoaded', async () => {
+
+    document.getElementById('crearEmpleado').addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        const identificacion = document.getElementById('identificacion').value;
+        const nombre = document.getElementById('nombre').value;
+        const apellido = document.getElementById('apellido').value;
+        const celular = document.getElementById('celular').value;
+        const direccion = document.getElementById('direccion').value;
+        const email = document.getElementById('email').value;
+        const contrasena = document.getElementById('contrasena').value;
+        const rol = document.getElementById('rol').value;
+
+        if (!identificacion || !nombre || !apellido|| !celular || !direccion || !email|| !contrasena || !rol) {
+            Swal.fire({
+                icon: 'error',
+                title: "<h5 style='color:white; font-family: 'Inter', sans-serif;'>" + 'Todos los campos son obligatorios' + "</h5>",
+                showConfirmButton: true,
+                customClass: {
+                    popup: 'bg-alert',
+                }
+            });
+            return;
+        }
+
+        // const token = sessionStorage.getItem("token");
+        const options = {
+            method: "POST",
+            headers:{
+                "content-Type": "application/json",
+                // "x-access-token": token
+            },
+            body:JSON.stringify({
+                identificacion: identificacion,
+                nombre: nombre,
+                apellido: apellido,
+                celular: celular,
+                direccion: direccion,
+                email: email,
+                contrasena: contrasena,
+                rol: rol
+            })
+        }
+
+        try {
+            const response = await fetch('http://localhost:3000/empleados/crear', options);
+
+            if (!response.ok) {
+                const responseText = await response.text();
+                throw new Error(`Error: ${response.status} ${response.statusText} - ${responseText}`);
+            }
+
+            const responseData = await response.json();
+            Swal.fire({
+                icon: 'success',
+                title: "<h5 style='color:white; font-family: 'Inter', sans-serif;'>" + 'Empleado agregada exitosamente' + "</h5>",
+                showConfirmButton: false,
+                timer: 1500,
+                customClass: {
+                    popup: 'bg-alert',
+                    content: 'text-alert'
+                }
+            });
+            setTimeout(() => {
+                window.location.href = `/admin/empleados`;
+            }, 1500);
+        } catch (error) {
+            console.error('Error al agregar empleado:', error);
+            Swal.fire({
+                icon: 'error',
+                title: "<h5 style='color:white; font-family: 'Inter', sans-serif;'>" + 'Error al agregar la oferta' + "</h5>",
+                showConfirmButton: true,
+                customClass: {
+                    popup: 'bg-alert',
+                }
+            });
+        }
+    });
+});
