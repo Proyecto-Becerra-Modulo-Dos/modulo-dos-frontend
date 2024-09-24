20eeaa74c47c71835adab433293aa9453369e888
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-09-23 21:09:26 -0500

    Adelanto de crear nuevo empleado y lista de empleados

--- src/controllers/adminrh.controllers.js
+++ src/controllers/adminrh.controllers.js
@@ -6,3 +6,11 @@ const url = process.env.BACKEND_URL
 export const principalAdminRH = (req, res) => {
     res.render("views.principal_admin_rh.ejs")
 }
+
+export const empleadosAdminRH = (req, res) => {
+    res.render("views.empleados_admin_rh.ejs")
+}
+
+export const crearEmpleadoAdminRH = (req, res) => {
+    res.render("views.crear_empleado.ejs")
+}
--- src/routes/routes.adminrh.js
+++ src/routes/routes.adminrh.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { principalAdminRH } from "../controllers/adminrh.controllers.js";
+import { crearEmpleadoAdminRH, empleadosAdminRH, principalAdminRH } from "../controllers/adminrh.controllers.js";
 
 const rutaAdminRH = Router();
 
 rutaAdminRH.get("/principal", principalAdminRH)
+rutaAdminRH.get("/empleados", empleadosAdminRH)
+rutaAdminRH.get("/crear-empleado", crearEmpleadoAdminRH)
 
 export default rutaAdminRH
