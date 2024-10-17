5503f8c629750c14ec72e6001d14496a24b8eddb
angienohava03 <angienohava03@gmail.com>
2024-10-16 22:37:24 -0500

    ventana de perfil lista

---
+++ src/public/js/popup_perfil.js
@@ -0,0 +1,26 @@
+const openButton = document.querySelector('.open-popup-button');
+const closeButton = document.querySelector('.close-button');
+const popup = document.querySelector('.popup');
+const accountButton = document.querySelector('.account-button');
+
+// Redirigir a empleado
+accountButton.addEventListener('click', function() {
+    window.location.href = '/empleado'; 
+});
+
+// abrir la ventana emergente 
+openButton.addEventListener('click', function() {
+    popup.style.display = 'flex';
+});
+
+// cerrar la ventana emergente 
+closeButton.addEventListener('click', function() {
+    popup.style.display = 'none';
+});
+
+// cerrar la ventana si se hace clic fuera 
+popup.addEventListener('click', function(e) {
+    if (e.target === popup) {
+        popup.style.display = 'none';
+    }
+});
